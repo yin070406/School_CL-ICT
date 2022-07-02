9b8e9eea4e2b3a21a9d59c6ab39e70795b915482
yin070406 <yin070406@gmail.com>
2022-07-02 14:31:55 +0800

    Update proj04_3B16_task2.py

--- proj04_3B16_task2.py
+++ proj04_3B16_task2.py
@@ -1,9 +1,15 @@
 # Coded by 3B16 Ng Tsz Yin
 
-# Importing Module "Random" & "Time".
+# Importing Module "Random" & "Time" & "String".
 # Their module will be used in the later program.
 import random
 import time
+import string
+
+# Variable "Sum" is the total marks
+sum = 0.0
+# Variable "numbers_count" is the total subjects
+numbers_count = 0
 
 # This is used for allow users to input value like numbers.
 # /
@@ -14,6 +20,27 @@ import time
 Chinese = float(input("Enter the Chinese Marks: "))
 English = float(input("Enter the English Marks: "))
 Maths = float(input("Enter the Maths Marks: "))
+
+# It means the sum of "sum + Chinese + English + Maths" 
+sum += Chinese + English + Maths
+# It means the total subjects amount of other subjects amount and Chinese, English and Maths
+# For example, i want to add 3 more subjects's marks, so the numbers_count will be 3 subjects + 3 (Chin, Eng, Maths) = 6
+numbers_count += 3
+
+# While means loop, after entering the value of Chinese, English and Maths Marks
+# The Variable "choice" is equal to ask user want to add another subjects marks or not
+while True:
+    choice = input("Add another subject's marks? ( Yes / No ) : ")
+    # If the user entered "Yes", the variable sum will equal to
+    # the input of other new subjects marks + sum
+    if choice == "Yes" :
+        # Add the other subject marks to sum
+        sum += float(input("Enter the Subjects Marks: "))
+        numbers_count += 1
+    # If the user entered "No"
+    # It will jump to the calculating part.
+    if choice == "No" :
+        break
 
 # This is a Variable that about the Easter Egg chances.
 # The chances of the Easter Egg are 0.10.
@@ -27,21 +54,21 @@ easter_egg_chances = 0.10
 # Also, I added a time delay here, the time.sleep() is the time delay function and (1) mean 1 second.
 # So basically, time.sleep(1) mean There is a 1 second time delay between three "Calculating the avergae mark." message.
 for i in range(1):
-    print("\nCalculating the avergae mark.")
+    print("\nCalculating the average mark.")
+    # The time delay function, 1 second delay
     time.sleep(1)
 for i in range(1):
-    print("Calculating the avergae mark..")
+    print("Calculating the average mark..")
     time.sleep(1)
 for i in range(1):
-    print("Calculating the avergae mark...")
+    print("Calculating the average mark...")
     time.sleep(1)
 
 # Average is the variable,
-# This is the process of calculating the mean of Chinese, English and Maths marks.
-# "(Chinese + English + Maths)/3" is the formula of calculating the mean of Chinese, English and Maths.
-average = (Chinese + English + Maths)/3
+# This is the process of calculating the mean of your subjects marks.
+average = sum / numbers_count
 
-# It is a 2 seconds time delay.
+# 2 second time delay 
 time.sleep(2)
 
 # "random.random()" means return a random floating number between 0 and 1.  
@@ -56,15 +83,16 @@ if random.random() < easter_egg_chances:
     time.sleep(2)
     print("Done fixing!")
     time.sleep(1)
-    print("\nRecalculating the avergae mark...")
+    print("\nRecalculating the average mark...")
 
-# It will print the two decimal places 
-# Average mark of Chinese, English and Maths.
+# f is f-string, and the words where inside the {} is the variable
+# It will print the value of the variable - Average mark of your subjects
 # /
 # This is the output.
-print("\nYour Average Mark is %.2f" %average)
+print(f"\nYour Total Mark is {sum}")
+print(f"Your Average Mark is {average}" )
 
-# It is a 1 seconds time delay (cooldown).
+# 1 second time delay
 time.sleep(1)
 
 # This is the if-statement.
@@ -74,7 +102,6 @@ time.sleep(1)
 # /
 # If the inputs are wrong or something else occurred like error or cannot calculate,
 # It will show "Unknown" in the console.
-
 if average >= 90:
     print( random.choice(["Excellent!", "Fantastic!", "Wow! You just did a Great Job!", "Impressive!", "Incredible!"]))
 elif average >= 70:
@@ -89,6 +116,3 @@ elif average < 0:
     print( random.choice(["Huh?", "Unknown.", "Error", "Error404 O_o"]))
 else:
     print("Unknown")
-
-
-
